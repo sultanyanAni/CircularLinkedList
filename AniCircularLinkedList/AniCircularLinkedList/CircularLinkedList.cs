a14b4277129d769e631311d7c0fd6f7e88894a98
Ani <sultanyanani@gmail.com>
2018-07-21 16:32:55 -0700

    Adding all files

---
+++ AniCircularLinkedList/AniCircularLinkedList/CircularLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniCircularLinkedList
+{
+    public class CircularLinkedList<T>
+    {
+        int Count = 0;
+        CircularLinkedListNode<T> Head;
+        CircularLinkedListNode<T> Tail;
+        public CircularLinkedList()
+        {
+
+        }
+        
+        //IsEmpty
+        public bool IsEmpty()
+        {
+            return 
+        }
+        //AddFirst
+        //AddEnd
+        //AddBefore
+        //AddAfter
+
+        //RemoveFirst
+        //RemoveLast
+        //RemoveAt
+    }
+}
---
+++ AniCircularLinkedList/AniCircularLinkedList/CircularLinkedListNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniCircularLinkedList
+{
+    public class CircularLinkedListNode<T>
+    {
+        public CircularLinkedListNode<T> Next { get; set; }
+        public CircularLinkedListNode<T> Previous { get; set; }
+        public T Value;
+        public CircularLinkedListNode(T ItemToAdd, CircularLinkedListNode<T> next = null, CircularLinkedListNode<T> previous = null)
+        {
+            Value = ItemToAdd;
+            Next = next;
+            Previous = previous;
+        }
+    }
+}
