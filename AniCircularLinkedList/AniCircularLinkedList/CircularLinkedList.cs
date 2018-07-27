b3a20770b5101a9f03b6b7f2c309be23764d8afc
Ani <sultanyanani@gmail.com>
2018-07-27 16:24:36 -0700

    added AddToFront and AddToEnd functions

--- AniCircularLinkedList/AniCircularLinkedList/CircularLinkedList.cs
+++ AniCircularLinkedList/AniCircularLinkedList/CircularLinkedList.cs
@@ -15,15 +15,85 @@ namespace AniCircularLinkedList
         {
 
         }
+
+        public CircularLinkedListNode<T> this[int index]
+        {
+            get
+            {
+                CircularLinkedListNode<T> currentNode = Head;
+                int count = 0;
+                while(count < index)
+                {
+                    currentNode = currentNode.Next;
+                }
+                return currentNode;
+            }
+            
+        }
         
         //IsEmpty
         public bool IsEmpty()
         {
-            return 
+            return Head == null;
+        }
+        public IEnumerator<CircularLinkedListNode<T>> GetEnumerator()
+        {
+            int count = 0;
+            CircularLinkedListNode<T> currentNode = Head;  
+            while(count != Count)
+            {
+                yield return currentNode;
+                currentNode = currentNode.Next;
+                count++;
+            }
         }
         //AddFirst
+        public void AddToFront(T itemToAdd)
+        {
+            if(IsEmpty())
+            {
+                Head = new CircularLinkedListNode<T>(itemToAdd);
+                Tail = Head;
+            }
+            else
+            {
+                CircularLinkedListNode<T> nodeToAdd = new CircularLinkedListNode<T>(itemToAdd);
+
+                nodeToAdd.Next = Head;
+                Head.Previous = nodeToAdd;
+                Head = nodeToAdd;
+                Head.Previous = Tail;
+                Tail.Next = Head;
+            }
+            Count++;
+        }
         //AddEnd
+        public void AddToEnd(T itemToAdd)
+        {
+            if(IsEmpty())
+            {
+                Head = new CircularLinkedListNode<T>(itemToAdd);
+                Tail = Head;
+               
+            }
+            else
+            {
+                Tail.Next = new CircularLinkedListNode<T>(itemToAdd, Head, Tail);
+                Tail = Tail.Next;
+
+            }
+            Count++;
+        }
         //AddBefore
+        public void AddAfterIndex(int index, T itemToAdd)
+        {
+            if(IsEmpty())
+            {
+                Head = new CircularLinkedListNode<T>(itemToAdd);
+                Tail = Head; 
+            }
+            else if()
+        }
         //AddAfter
 
         //RemoveFirst
--- AniCircularLinkedList/AniCircularLinkedList/Program.cs
+++ AniCircularLinkedList/AniCircularLinkedList/Program.cs
@@ -10,6 +10,55 @@ namespace AniCircularLinkedList
     {
         static void Main(string[] args)
         {
+            CircularLinkedList<int> list = new CircularLinkedList<int>();
+            bool keepLooping = true;
+            while (keepLooping)
+            {
+                string response = "";
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1) Add to list");
+                Console.WriteLine("2) Remove from list");
+                Console.WriteLine("3) View the list");
+                Console.WriteLine("4) Exit");
+
+                response = Console.ReadLine();
+
+                switch (response)
+                {
+                    case "1":
+                        Console.WriteLine("What item would you like to add?");
+                        int itemToAdd = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Where would you like to add the item?");
+                        Console.WriteLine("1) The Front");
+                        Console.WriteLine("2) The End");
+                        Console.WriteLine("3) Before an index");
+                        Console.WriteLine("4) After an index");
+                        response = Console.ReadLine();
+                        switch (response)
+                        {
+                            case "1":
+                                list.AddToFront(itemToAdd);
+                                break;
+                            case "2":
+                                list.AddToEnd(itemToAdd);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case "3":
+                        int count = 1;
+                        foreach (CircularLinkedListNode<int> node in list)
+                        {
+                            Console.WriteLine($"{count}) {node.Value}");
+                            count++;
+                        }
+                        break;
+                    case "4":
+                        keepLooping = false;
+                        break;
+                }
+            }
         }
     }
 }
